b60ef81ed7eb6eea29e19527b352796c71925ca3
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-12-17 10:59:18 +0600

    Update LoginActivity.java

--- LoginActivity.java
+++ LoginActivity.java
@@ -9,3 +9,15 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText edit_phone, enter_iin;
+    private String phone_num;
+    private CheckBox checkBox;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String PHONE_NUMBER = "phoneNumber";
+    public static final String IIN = "iiN";
+
+    private String phoneNumberS, iinS;
