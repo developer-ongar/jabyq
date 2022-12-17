56e32d4902932a480adb9ad10f8b1b65a8c82cd1
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-12-17 10:58:53 +0600

    Create LoginActivity.java

---
+++ LoginActivity.java
@@ -0,0 +1,11 @@
+package com.example.jabyq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
